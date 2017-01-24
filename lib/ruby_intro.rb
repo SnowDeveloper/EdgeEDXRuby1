2e1f42d87c5c24d2faf23f1289582f885580b971
SnowDeveloper <contact.madhushreesingh@gmail.com>
2017-01-24 23:05:34 +0200

    Initial Commit:

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,113 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) {|sum,a| sum + a.to_i }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+  	return 0
+  else
+  	arr.max(2).reduce(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? {|a, b| (a + b) == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  n = "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # We check for empty input
+  if s == ""
+  	return false
+  end
+
+  # We check if it is a non-letter
+  if s[0][/[a-zA-Z]+/] != s[0]
+  	return false
+  end
+  
+  # We check if it is a vowel or a consonant
+  if s[0] == 'a' || s[0] == 'e' || s[0] == 'i' || s[0] == 'o' || s[0] == 'u' || 
+  	s[0] == 'A' || s[0] == 'E' || s[0] ==  'I' || s[0] ==  'O' || s[0] == 'U' 
+  	return false
+  else  	
+  	return true
+  end	
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # Helpful snippet from stackoverflow
+  def number_or_nil(string)
+  	Integer(string || '')
+  rescue ArgumentError
+  	nil
+  end
+
+  # Converting and checking if input is a number
+  n = number_or_nil(s)
+  if n == nil
+  	return false
+  end
+
+  # Checking whether not divisible by 4
+  if n%4 != 0
+  	return false
+  end
+
+  # Since valid number, and divisible by therefore it is true
+  return true
 end
 
 # Part 3
-
 class BookInStock
-# YOUR CODE HERE
+	# Getters and Setters for class variable isbn
+	attr_reader :isbn
+	attr_writer :isbn
+
+	# Getters and Setters for class variable price
+	attr_reader :price
+	attr_writer :price
+
+	def initialize(isbn, price)
+		@isbn = isbn
+		@price = price
+
+		# Checking if ISBN number is empty
+		if isbn == ""
+		    raise ArgumentError, "ISBN number is invalid, it must not be empty"
+		    return
+		end
+
+		# Checking if price is a valid number
+		p = number_or_nil(price)
+		if p == nil 
+			raise ArgumentError, "Price is invalid, it should only contain digits"
+			return
+		end
+
+		# Checking if price is less than 1
+		if p <= 0
+			raise ArgumentError, "Price is invalid, it should be greater than 0"
+			return
+		end
+	end
+
+	def price_as_string()
+		sprintf "$%.2f" % @price
+	end
+
+	# Helpful snippet from stackoverflow
+  	def number_or_nil(string)
+  		Integer(string || '')
+  	rescue ArgumentError
+  		nil
+  	end
 end
